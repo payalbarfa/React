8c27edc997a64682b4e2c43043adf82560ec01df
payalbhayal <55580235+payalbhayal@users.noreply.github.com>
2023-10-11 14:05:30 +0530

    Nameste React Episodes

---
+++ episode 2/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+
+const heading = React.createElement("h1",{id:"heading", xyz:"abc"},"Hello World From React😍");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
---
+++ episode 2/README.md
@@ -0,0 +1,1 @@
+Nameste React Episode 2
